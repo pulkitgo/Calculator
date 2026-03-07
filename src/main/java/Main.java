96579acc95410fdcae7d30d87e0898f329a51dd9
pulkitgo <Pulkit.Goyal@iiitb.ac.in>
2026-03-08 01:18:39 +0530

    first

---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+    public double sqrt ( double x){
+        return Math.sqrt(x);
+    }
+
+    public double factorial ( double x){
+        if (x < 0) throw new IllegalArgumentException("Negative not allowed");
+
+        long res = 1;
+        for (int i = 2; i <= x; i++) {
+            res *= i;
+        }
+        return res;
+    }
+
+    public double ln ( double x){
+        if (x <= 0) throw new IllegalArgumentException("x must be positive");
+        return Math.log(x);
+    }
+
+    public double power ( double x, double b){
+        return Math.pow(x, b);
+    }
+    public static void main(String[] args)
+    {
+        Main calculator = new Main();
+        System.out.println("sqrt(16) = " + calculator.sqrt(16));
+        System.out.println("factorial(5) = " + calculator.factorial(5));
+        System.out.println("ln(10) = " + calculator.ln(10));
+        System.out.println("power(2,3) = " + calculator.power(2,3));
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest
+{
+    Main calc = new Main();
+
+    // √x
+    @Test
+    void testSquareRoot() {
+        assertEquals(5.0, calc.sqrt(25), 0.0001);
+        assertEquals(10000, calc.sqrt(100000000),0.0001);
+        assertEquals(9.9, calc.sqrt(98.01), 0.0001);
+        assertEquals(89766.89899, calc.sqrt(8058096154.28),0.0001);
+    }
+
+    // x!
+    @Test
+    void testFactorial() {
+        assertEquals(1, calc.factorial(0));
+        assertEquals(5040, calc.factorial(7));
+        assertEquals(40320, calc.factorial(8));
+        assertEquals(2, calc.factorial(2));
+    }
+
+    // ln(x)
+    @Test
+    void testNaturalLog() {
+        assertEquals(1.0, calc.ln(Math.E), 0.0001);
+        assertEquals(15.4249484704, calc.ln(5000000), 0.0001);
+        assertEquals(20.7232658359, calc.ln(999999999), 0.0001);
+        assertEquals(0.0, calc.ln(1), 0.0001);
+    }
+
+    // x^b
+    @Test
+    void testPower() {
+        assertEquals(8.0, calc.power(2,3), 0.0001);
+        assertEquals(3125.0, calc.power(5,5), 0.0001);
+        assertEquals(65536.0 , calc.power(2,16), 0.0001);
+        assertEquals(1.0 , calc.power(1,100000000), 0.0001);
+    }
+
+}

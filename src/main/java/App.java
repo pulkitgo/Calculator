4f83e6f17be2593949b3c4bf29097a7a5ab97312
Pulkit goyal <222521487+pulkitgo@users.noreply.github.com>
2026-03-07 11:52:53 +0530

    Add files via upload

---
+++ src/main/java/App.java
@@ -0,0 +1,25 @@
+public class App {
+
+    public double sqrt(double x) {
+        return Math.sqrt(x);
+    }
+
+    public double factorial(double x) {
+        if (x < 0) throw new IllegalArgumentException("Negative not allowed");
+
+        long res = 1;
+        for (int i = 2; i <= x; i++) {
+            res *= i;
+        }
+        return res;
+    }
+
+    public double ln(double x) {
+        if (x <= 0) throw new IllegalArgumentException("x must be positive");
+        return Math.log(x);
+    }
+
+    public double power(double x, double b) {
+        return Math.pow(x, b);
+    }
+}
